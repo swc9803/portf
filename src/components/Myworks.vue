338fe7d2612e3bdcfbab92338d77d3037a08323e
swc9803 <swc9803@gmail.com>
2022-02-08 04:22:08 +0900

    myworks 설명 추가 / 사이트, 깃헙 이동 이벤트, 사진 추가

--- src/components/Myworks.vue
+++ src/components/Myworks.vue
@@ -2,7 +2,6 @@
   <p class="title">My Works</p>
   <div class="projectInfo">
     <p class="projectTitle">계란 판매 사이트</p>
-    <p class="projectTitleDes">디자인 100% </p>
     <div class="sliderWrap">
       <swiper class="slider"
         :slidesPerView="1"
@@ -16,8 +15,14 @@
         <swiper-slide class="dd yellow">Slide 3</swiper-slide>
         <swiper-slide class="dd green">Slide 4</swiper-slide>
       </swiper>
-      <div class="projectDes">hi</div>
-      <div class="projectDes">hi</div>
+      <div class="projectDes">
+        <div>가장 최근에 제작한 사이트입니다. 여러 사이트를 제작해보며 반응형 웹과 애니메이션이 부족하다고 느껴 본 사이트는 반응형 레이아웃과 애니메이션에 집중을 하며 제작을 하였습니다.</div>
+        <div>hi</div>
+        <div class="icons">
+          <img src="@/assets/homeIcon.png" @click="goToEggSite">
+          <img src="@/assets/githubIcon.png" @click="goToEggGit">
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -34,10 +39,18 @@ export default {
     SwiperSlide
   },
   setup () {
+    const goToEggSite = () => {
+      open('https://segg-1.firebaseapp.com/')
+    }
+    const goToEggGit = () => {
+      open('https://github.com/swc9803/eggtest')
+    }
     onMounted(() => {
 
     })
     return {
+      goToEggSite,
+      goToEggGit,
       modules: [Pagination]
     }
   }
@@ -52,6 +65,7 @@ export default {
   transform: translate(-50%);
   left: 50%;
   width: 70%;
+  box-shadow: 0 0 0 2px rgb(206, 192, 68) inset;
   background: rgb(255, 255, 255);
   border-radius: 10px;
   overflow: hidden;
@@ -61,30 +75,36 @@ export default {
     font-weight: 600;
     letter-spacing: 2px;
   }
-  .projectTitleDes {
-    text-align: center;
-  }
   .sliderWrap {
     display: flex;
     flex-flow: row;
-  }
-  .slider {
-    position: relative;
-    // transform: translate(-50%, -50%);
-    // top: 50%;
-    left: 0%;
-    text-align: left;
-    width: 60%;
-    margin: 20px;
-    height: 50vh;
-    overflow: hidden;
-    cursor: pointer;
-    // dd {
-    // }
-  }
-  .projectDes {
-    width: 50%;
-    margin-top: 20px;
+    .slider {
+      flex: 1 1 45%;
+      margin: 20px;
+      height: 50vh;
+      overflow: hidden;
+      cursor: pointer;
+    }
+    .projectDes {
+      flex: 1 1 45%;
+      margin: 20px;
+      margin-top: 20px;
+      word-break: keep-all;
+      font-size: 1.2em;
+      .icons {
+        text-align: center;
+        margin-top: 20px;
+        img {
+          cursor: pointer;
+          width: 25px;
+          margin: 15px;
+          transition: .2s;
+          &:hover {
+            transform: translateY(-3px);
+          }
+        }
+      }
+    }
   }
   .blue {
     background: blue;
